09d6309bec94c07a9deeb68a9854a73bfbe28a68
jdeon <jmdeon@gmail.com>
2016-09-15 12:18:28 -0700

    add JSON tokenizer

--- src/main/scala/JSONToJava/JSONTokenizer.scala
+++ src/main/scala/JSONToJava/JSONTokenizer.scala
@@ -2,4 +2,26 @@ object JSONTokenizer{
 
   def hello = println("hello")
 
+  def tokenize(json: String):List[String] = {
+    def tokenizeRec(remaining: List[String], acc: List[String], acc2: String): List[String] ={
+      if(remaining.isEmpty){
+        if(acc2.isEmpty) acc
+        else acc2::acc
+      }
+      else{
+        def h = remaining.head
+        def t = remaining.tail
+        def tokenRecurse = if(acc2.isEmpty) tokenizeRec(t, h::acc, "") else tokenizeRec(t, h::acc2::acc, "")
+        def spaceRecurse = if(acc2.isEmpty) tokenizeRec(t, acc, "") else tokenizeRec(t, acc2::acc, "")
+        h match{
+          case "{" | "}" | "[" | "]" | ":" | "," => tokenRecurse
+          case " "  => spaceRecurse
+          case  x  => tokenizeRec(t, acc, acc2 + x)
+        }
+      }
+    }
+    tokenizeRec(json.split("").toList, List(), "").reverse
+  }
+
+
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,6 @@
 import JSONTokenizer._
 object Main{
 
-  def main(args: Array[String]) = JSONTokenizer.hello
+  def main(args: Array[String]) = println(JSONTokenizer.tokenize("{\"hello\":goodbye}"))
 
 }
